e7249aa458d7c02b2617169a581b109053145e02
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-20 06:17:32 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /* main -Positive anything is better than negative nothing */
-/* return (0); Success */
+/* Return (0); Success */
+
 int main(void)
 {
 	int n;
