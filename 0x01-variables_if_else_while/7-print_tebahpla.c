72c25192a8734ee18cbea02df927446ab36ec0c9
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-21 02:16:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
