7ef6adcc4f73c3d92238b2031c571f1412a5fcde
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-21 16:44:38 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower- checks for lower characters.
+ * Return:1 if true and 0 if false .
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
