31307911a3f6fcb88d44400a18bd57a097f29c08
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-21 14:35:32 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+        {
+               _putchar(c);
+                c++;
+        }
+	_putchar('\n');
+}
+
