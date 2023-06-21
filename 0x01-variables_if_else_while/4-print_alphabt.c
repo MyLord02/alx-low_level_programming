a0ba9c74d351583453c3d7b86832c1082fc5a1e7
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-21 00:58:41 -0700

    4-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n != 'q' && n != 'e')
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
