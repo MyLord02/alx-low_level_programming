d5ad59cf82a623676358124130a6d4a1c29d50a5
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-27 13:25:29 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@ char *_strcpy(char *dest, char *src)
 	int a = 0;
 	int b = 0;
 
-	while (*(str + 1) != '\0')
+	while (*(src + 1) != '\0')
 	{
 		a++;
 	}
 	for ( ; b < a; b++)
 	{
-		dest[b] = sr[b];
+		dest[b] = src[b];
 	}
 	dest[a] = '\0';
 	return (dest);
