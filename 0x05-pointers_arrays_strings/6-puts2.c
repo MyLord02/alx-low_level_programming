371f8c3e707f7cf2a4f7004a9d78c228ee8cde2c
MyLord02 <xolaniximb08127a@gmail.com>
2023-06-27 11:53:04 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - function which prins one character 
+ *@str: input string 
+ * Return: output
+ */
+void puts2(char *str)
+{
+	int values = 0;
+	char *new = str;
+	int counter = 0;
+	int counter2 = 0;
+
+	while (*new != '\0' )
+	{
+		*new++;
+		counter++;
+	}
+	values = counter -1;
+	for ( counter2 = 0 ; counter2 <= counter ; counter2++)
+	{
+		if (counter2 % 2 == 0)
+	{
+		_putchar(str[counter2]);
+	}
+	}
+	_putchar('\n');
+}
