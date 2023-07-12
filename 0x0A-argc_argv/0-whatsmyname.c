0f6135a4ad973f221f178b9c79c54bb7aec5a8fe
MyLord02 <xolaniximb08127a@gmail.com>
2023-07-12 13:57:31 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *main - prints its name, followed by a new line.
+  *@argv : the string from the terminal
+  *@argc : the number of arameters
+  *Return: Always 0 (Success)
+  */
+
+int main(int argc__attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
